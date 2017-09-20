7ae9cfeb16d1e0debdf1aaeab0b9d907379bea46
Vadim <muller95@yandex.ru>
2017-09-20 21:36:36 +0300

    ba2veda

--- src/main/java/sm/tools/ba2veda/impl/_b4a29_mnd_s_ConatractDirection.java
+++ src/main/java/sm/tools/ba2veda/impl/_b4a29_mnd_s_ConatractDirection.java
@@ -7,7 +7,7 @@ import sm.tools.veda_client.VedaConnection;
 
 public class _b4a29_mnd_s_ConatractDirection extends Ba2VedaTransform {
 	public _b4a29_mnd_s_ConatractDirection(BaSystem _ba, VedaConnection _veda, Replacer replacer) {
-		super(_ba, _veda, replacer, "b4a2927d05e54442ba78f5e8ec32bd9d", "mnd-s:ConatractDirection");
+		super(_ba, _veda, replacer, "b4a2927d05e54442ba78f5e8ec32bd9d", "mnd-s:ContractDirection");
 	}
 	
 	public void inital_set() {
