9d393aaf639671b4597818ed65eb752354a68a25
Vadim <muller95@yandex.ru>
2018-03-06 11:50:37 +0300

    ba2veda

--- src/main/java/sm/tools/ba2veda/impl/_b163ab_mnd_s_TechnicalDocument.java
+++ src/main/java/sm/tools/ba2veda/impl/_b163ab_mnd_s_TechnicalDocument.java
@@ -95,24 +95,36 @@ public class _b163ab_mnd_s_TechnicalDocument extends Ba2VedaTransform
 							recordId2 = ba.get_first_value_of_field(recordDoc, "Раздел");
 						if (recordId2 == null)
 							recordId2 = "";
-						if (recordId.equals("69ca82170be24d41b32fc9033a2574f5"))
+						if (recordId.equals("69ca82170be24d41b32fc9033a2574f5") ||
+							recordId2.equals("69ca82170be24d41b32fc9033a2574f5")) {
 							new_individual.addProperty("mnd-s:hasMarkOfTechnicalDocuments", rss);
-						else if (recordId2.equals("4f391bc4b9434d619ea95396cd0faba7")) {
+							new_individual.addProperty("v-s:hasDocumentKind", 
+								new Resource("d:afc1a827f2ac47a9bd19b6db910dfc13", Type._Uri));
+						} else if (recordId2.equals("4f391bc4b9434d619ea95396cd0faba7")) {
 							new_individual.addProperty("mnd-s:hasSectionOfProjectDocumentation", rss);
-							new_individual.addProperty("v-s:hasDocumentKind", new Resource("d:kqyyu62f90hy89wh188664bxwl", Type._Uri));
-						} else if (recordId.equals("7d67bd472db4481db0f5511f37107cae") || recordId.equals("db6c04d678c849859295f65efce1de76")) {
+							new_individual.addProperty("v-s:hasDocumentKind", 
+								new Resource("d:kqyyu62f90hy89wh188664bxwl", Type._Uri));
+						} else if (recordId.equals("7d67bd472db4481db0f5511f37107cae") || 
+							recordId.equals("db6c04d678c849859295f65efce1de76")) {
 							new_individual.addProperty("mnd-s:hasMarkOfTechnicalDocuments", 
 								new Resource("d:6ba70b2261d4443e98d91452565d3b98", Type._Uri));
-							new_individual.addProperty("v-s:hasDocumentKind", new Resource("d:mqzlxqrejhhbod4ra42nq8cf", Type._Uri));
+							new_individual.addProperty("v-s:hasDocumentKind", 
+								new Resource("d:mqzlxqrejhhbod4ra42nq8cf", Type._Uri));
 						} else if (recordId.equals("6ba70b2261d4443e98d91452565d3b98")) {
-							new_individual.addProperty("v-s:hasDocumentKind", new Resource("d:afc1a827f2ac47a9bd19b6db910dfc13", Type._Uri));
-							new_individual.addProperty("mnd-s:hasMarkOfTechnicalDocuments", new Resource("d:6ba70b2261d4443e98d91452565d3b98", Type._Uri));
+							new_individual.addProperty("v-s:hasDocumentKind", 
+								new Resource("d:afc1a827f2ac47a9bd19b6db910dfc13", Type._Uri));
+							new_individual.addProperty("mnd-s:hasMarkOfTechnicalDocuments", 
+								new Resource("d:6ba70b2261d4443e98d91452565d3b98", Type._Uri));
 						} else if (recordId.equals("584e7ef299b14bec89c516b311472ba5")) {
-							new_individual.addProperty("v-s:hasDocumentKind", new Resource("d:99d3887ae22d439c9fe77a10ff5a4b0d", Type._Uri));
-							new_individual.addProperty("mnd-s:hasMarkOfTechnicalDocuments", new Resource("d:6ba70b2261d4443e98d91452565d3b98", Type._Uri));
+							new_individual.addProperty("v-s:hasDocumentKind", 
+								new Resource("d:99d3887ae22d439c9fe77a10ff5a4b0d", Type._Uri));
+							new_individual.addProperty("mnd-s:hasMarkOfTechnicalDocuments", 
+								new Resource("d:6ba70b2261d4443e98d91452565d3b98", Type._Uri));
 						} else if (recordId.equals("41b925655c8a44a8b34ab3b1894bebd0")) {
-							new_individual.addProperty("v-s:hasDocumentKind", new Resource("d:41b925655c8a44a8b34ab3b1894bebd0", Type._Uri));
-							new_individual.addProperty("mnd-s:hasMarkOfTechnicalDocuments", new Resource("d:zn8jlec6ma6x28fsgp6lyw49zo", Type._Uri));
+							new_individual.addProperty("v-s:hasDocumentKind", 
+								new Resource("d:41b925655c8a44a8b34ab3b1894bebd0", Type._Uri));
+							new_individual.addProperty("mnd-s:hasMarkOfTechnicalDocuments", 
+								new Resource("d:zn8jlec6ma6x28fsgp6lyw49zo", Type._Uri));
 						}
 					}
 				} else if (code.equals("Разработчик")) {
